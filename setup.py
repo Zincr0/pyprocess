5428ecaf80e68df481da5f61c98e2696423b1977
Daniel Mondaca <daniel@analitic.cl>
2012-06-01 11:29:48 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "pyutils",
+    name = "pyprocess",
     version = "0.0.1",
     author = "Daniel Mondaca",
     author_email = "dnielm@gmail.com",
     description = ("process name managment."),
-    license = "New Bsd License",
+    license = "Bsd",
     keywords = "process name",
     url = "https://github.com/Nievous/pyprocess",
     packages=['pyprocess'],
@@ -25,7 +25,10 @@ setup(
     package_data = {'package' : files },
     classifiers=[
         "Development Status :: 0 - Alpha",
-        "Topic :: single process name",
-        "License :: New Bsd License",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Topic :: Software Development" 
     ],
 )
